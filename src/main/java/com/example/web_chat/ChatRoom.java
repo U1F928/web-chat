0dca1c39b7255ae89ca12f4e49886ffc1a743b67
none <none>
2023-02-08 22:41:22 +0100

    added ChatRoom class

---
+++ src/main/java/com/example/web_chat/ChatRoom.java
@@ -0,0 +1,22 @@
+package com.example.web_chat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "chat_rooms")
+class ChatRoom
+{
+    @Id
+    @Column(name = "chat_room_name")
+    private String roomName;
+
+    public ChatRoom(){};
+
+    public ChatRoom(String roomName)
+    {
+        this.roomName = roomName;
+    }
+}
