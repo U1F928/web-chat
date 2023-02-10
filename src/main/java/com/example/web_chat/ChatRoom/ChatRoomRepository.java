4a899572d2ce1ff36521af323866ec1ba0470fac
none <none>
2023-02-10 21:51:27 +0100

    added comment, explaining function name derived query

--- src/main/java/com/example/web_chat/ChatRoom/ChatRoomRepository.java
+++ src/main/java/com/example/web_chat/ChatRoom/ChatRoomRepository.java
@@ -2,6 +2,14 @@ package com.example.web_chat.ChatRoom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
+ * 
+ * The repository proxy has two ways to derive a store-specific query from the method name:
+ *  - By deriving the query from the method name directly.
+ *  - By using a manually defined query.
+ * 
+ */
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String>
 {
     ChatRoom findByRoomName(String roomName);
