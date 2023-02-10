6c19695f9cfbef560cc71669728ea67bd4aef424
none <none>
2023-02-10 21:50:46 +0100

    added queries to ChatMessageRepository to get messages by timestamp

--- src/main/java/com/example/web_chat/ChatMessage/ChatMessageRepository.java
+++ src/main/java/com/example/web_chat/ChatMessage/ChatMessageRepository.java
@@ -1,7 +1,20 @@
 package com.example.web_chat.ChatMessage;
 
+import java.util.ArrayList;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
+ * 
+ * The repository proxy has two ways to derive a store-specific query from the method name:
+ *  - By deriving the query from the method name directly.
+ *  - By using a manually defined query.
+ * 
+ */
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>
 {
+    ArrayList<ChatMessage> findByUnixTimestampLessThan(long unixTimestamp, Pageable pageable);
+    ArrayList<ChatMessage> findByUnixTimestampGreaterThan(long unixTimestamp, Pageable pageable);
 }
