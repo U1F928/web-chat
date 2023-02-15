6a0fcc7b39f310e63dbcd5ee82d6fb9d4a18ca34
none <none>
2023-02-15 20:49:46 +0100

    removed unused import

--- src/main/java/com/example/web_chat/MessageRequest/MessageRequestService.java
+++ src/main/java/com/example/web_chat/MessageRequest/MessageRequestService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.web_chat.ChatMessage.ChatMessage;
