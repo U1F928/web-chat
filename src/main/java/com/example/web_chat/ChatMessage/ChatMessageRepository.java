51ad008100cd52faba91a8e5eb7c5820aae5297f
none <none>
2023-02-17 22:09:21 +0100

    removed unnecessary null check

--- src/main/java/com/example/web_chat/ChatMessage/ChatMessageRepository.java
+++ src/main/java/com/example/web_chat/ChatMessage/ChatMessageRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.example.web_chat.ChatRoom.ChatRoom;
+
 /*
  * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details
  * 
@@ -16,9 +18,9 @@ import org.springframework.data.jpa.repository.Query;
  */
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>
 {
-    @Query("SELECT c FROM ChatMessage c WHERE c.unixTimestamp <= :unixTimestamp ORDER BY c.unixTimestamp DESC, c.id DESC")
-    ArrayList<ChatMessage> findByUnixTimestampLessThan(long unixTimestamp, Pageable pageable);
+    @Query("SELECT c FROM ChatMessage c WHERE c.room = :room AND c.unixTimestamp <= :unixTimestamp ORDER BY c.unixTimestamp DESC, c.id DESC")
+    ArrayList<ChatMessage> findByUnixTimestampLessThan(ChatRoom room, long unixTimestamp, Pageable pageable);
 
-    @Query("SELECT c FROM ChatMessage c WHERE c.unixTimestamp >= :unixTimestamp ORDER BY c.unixTimestamp ASC, c.id ASC")
-    ArrayList<ChatMessage> findByUnixTimestampGreaterThan(long unixTimestamp, Pageable pageable);
+    @Query("SELECT c FROM ChatMessage c WHERE c.room = :room AND c.unixTimestamp >= :unixTimestamp ORDER BY c.unixTimestamp ASC, c.id ASC")
+    ArrayList<ChatMessage> findByUnixTimestampGreaterThan(ChatRoom room, long unixTimestamp, Pageable pageable);
 }
--- src/main/java/com/example/web_chat/MessageRequest/MessageRequestService.java
+++ src/main/java/com/example/web_chat/MessageRequest/MessageRequestService.java
@@ -1,5 +1,6 @@
 package com.example.web_chat.MessageRequest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import com.example.web_chat.ChatMessage.ChatMessage;
 import com.example.web_chat.ChatMessage.ChatMessageRepository;
+import com.example.web_chat.ChatRoom.ChatRoom;
+import com.example.web_chat.ChatRoom.ChatRoomRepository;
 
 @Service
 public class MessageRequestService
@@ -17,19 +20,23 @@ public class MessageRequestService
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
     public List<ChatMessage> process(String roomName, MessageRequest messageRequest)
     {
+        ChatRoom chatRoom = this.chatRoomRepository.findByRoomName(roomName);
         if(messageRequest.getRequestType() == MessageRequestType.LESS_THAN_TIMESTAMP)
         {
             Pageable firstN = PageRequest.of(0, messageRequest.getMessageCount());
-            List<ChatMessage> requestedMessages = this.chatMessageRepository.findByUnixTimestampLessThan(messageRequest.getUnixTimestamp(), firstN);
+            List<ChatMessage> requestedMessages = this.chatMessageRepository.findByRoomAndUnixTimestampLessThan(chatRoom, messageRequest.getUnixTimestamp(), firstN);
             Collections.reverse(requestedMessages);
             return requestedMessages;
         }
         else if(messageRequest.getRequestType() == MessageRequestType.GREATER_THAN_TIMESTAMP)
         {
             Pageable firstN = PageRequest.of(0, messageRequest.getMessageCount());
-            return this.chatMessageRepository.findByUnixTimestampGreaterThan(messageRequest.getUnixTimestamp(), firstN);
+            return this.chatMessageRepository.findByRoomAndUnixTimestampGreaterThan(chatRoom, messageRequest.getUnixTimestamp(), firstN);
         }
 
         return null;
