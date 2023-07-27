8b7cd0c299beaed3866d133ca9845d6b5d565a75
none <none>
2023-07-27 22:07:09 +0200

    added functionality: Clear text area upon sending message

--- chat-app/app/src/components/ChatRoom/ChatRoom.tsx
+++ chat-app/app/src/components/ChatRoom/ChatRoom.tsx
@@ -1,68 +1,91 @@
 import { useParams } from "react-router-dom"
 import './ChatRoom.css'
 import { Client } from '@stomp/stompjs';
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function ChatRoom()
 {
-	let roomName : string = useParams().roomName as string;
-	let client : Client;
+	let roomName: string = useParams().roomName as string;
+	const [messageFormText, setMessageFormText] = useState("");
+	let client = useRef(new Client());
+	let wasRenderedBefore = useRef(false);
+	// TODO: use 'state' to represent the message-section, 
+	//  	 also use 'state' to represent the message-form textarea
+	// TODO: add functionality to load older messages via requests by timestamp
+	//
+	// TODO: add send by hitting enter functionality
 	function initializeConnection()
 	{
-		client = new Client
-		(
-			{
-				brokerURL: 'ws://localhost:8080/websocket',
-				debug: (str) => 
+		client.current = new Client
+			(
 				{
-					console.log(str);
-				},
-				onConnect: () => 
-				{
-					client.subscribe
-					(
-						'/topic/room.' + roomName, 
-						(message) =>
-						{
-							console.log(`Received: ${message.body}`);
-						}
-					);
+					brokerURL: 'ws://localhost:8080/websocket',
+					debug: (str) => 
+					{
+						console.log(str);
+					},
+					onConnect: () => 
+					{
+						client.current.subscribe
+							(
+								'/topic/room.' + roomName,
+								(message : any) =>
+								{
+									console.log(`Received: ${message.body}`);
+								}
+							);
+					}
 				}
-			}
-		);
-	
-		client.activate();
+			);
+
+		client.current.activate();
 		console.log("initializing...");
 	}
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>)
-    {
-        event.preventDefault();
-        const messageInput : HTMLInputElement = event.currentTarget.elements.namedItem("message-form") as HTMLInputElement;
-        const message : string  = messageInput.value;
-        let clientMessage = { "text": message };
-		client.publish({ destination: "/app/room/" + roomName + "/publish_message", body: JSON.stringify(clientMessage) })
-    }
-	useEffect(initializeConnection);
-    return(
-        <div id="chat">
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>)
+	{
+		event.preventDefault();
+		const messageInput: HTMLInputElement = event.currentTarget.elements.namedItem("message-form") as HTMLInputElement;
+		const message: string = messageInput.value;
+		let clientMessage = { "text": message };
+		client.current.publish({ destination: "/app/room/" + roomName + "/publish_message", body: JSON.stringify(clientMessage) })
+		setMessageFormText("");
+	}
+	function handleChange(event : any)
+	{
+		setMessageFormText(event.currentTarget.value);
+	}
+	useEffect
+	(
+		() =>
+		{
+			console.log("was rendered before:" + wasRenderedBefore.current);
+			if(wasRenderedBefore.current === false)
+			{
+				initializeConnection();
+				wasRenderedBefore.current = true;
+			}
+		}
+	);
+	return (
+		<div id="chat">
 			<div id="room-name">
-  	          {roomName}
+				{roomName}
 			</div>
 
 			<div id="message-section">
-  	      {/*
+				{/*
 				<img src="/static/chat/loading_icon.svg" alt="Loading..." id="loading-icon">
   	          </img>
   	      */}
 			</div>
 
-        	<form id="send-message" onSubmit={handleSubmit}>
-				<textarea form="send-message" id="message-form" rows={1} name="message_text" placeholder="Aa"/> 
+			<form id="send-message" onSubmit={handleSubmit}>
+				<textarea form="send-message" id="message-form" rows={1} name="message_text" placeholder="Aa" onChange={handleChange} value={messageFormText} />
 				<button id="send-button"> </button>
-        	</form>
+			</form>
 
 		</div>
-    )
+	)
 }
 
 export default ChatRoom
