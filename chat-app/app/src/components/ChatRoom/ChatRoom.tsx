1849e5ed99b29af9776541df471bfb9a39660e43
none <none>
2023-07-29 22:58:19 +0200

    added a friendly scrolling behaviour when new messages are added

--- chat-app/app/src/components/ChatRoom/ChatRoom.tsx
+++ chat-app/app/src/components/ChatRoom/ChatRoom.tsx
@@ -11,11 +11,21 @@ function ChatRoom()
 	const [messages, setMessages] = useState<any[]>([]);
 	let client = useRef(new Client());
 	let wasRenderedBefore = useRef(false);
+	let initTimestamp = useRef(Date.now());
+	let lastRequestedPageNumber = useRef(-1);
 	// TODO: add functionality to load older messages via requests by timestamp
 	//
 	// TODO: add send by hitting enter functionality
 	//
 	// TODO: use CSS modules https://medium.com/@ralph1786/using-css-modules-in-react-app-c2079eadbb87
+
+	function createNewMessageElement(message : any)
+	{
+		let messageText = JSON.parse(message.body).text;
+		let messageID = JSON.parse(message.body).id;
+		let messageElement = createElement( 'div', { id: messageID, className: 'message', key : messageID}, messageText);
+		return messageElement;
+	}
 	function handleConnect() 
 	{
 		client.current.subscribe
@@ -23,9 +33,7 @@ function ChatRoom()
 			`/topic/room.${roomName}`,
 			function handleRecievedMessage(message : any)
 			{
-				let messageText = JSON.parse(message.body)["text"];
-				let messageID = JSON.parse(message.body)["id"];
-				let messageElement = createElement( 'div', { className: 'message', key : messageID}, messageText);
+				let messageElement = createNewMessageElement(message);
 				//https://stackoverflow.com/questions/59322030/why-is-react-statearray-empty-inside-callback-function-why-is-it-not-using-th
 				// use state updater function
 				function updateMessages(oldMessages : any)
@@ -36,6 +44,23 @@ function ChatRoom()
 				console.log(`Received: ${message.body}`);
 			}
 		);
+		//client.current.subscribe
+		//(
+		//	`/user/topic/requested_messages`,
+		//	function handleRecievedMessage(message : any)
+		//	{
+		//		let messageElement = createNewMessageElement(message);
+		//		//https://stackoverflow.com/questions/59322030/why-is-react-statearray-empty-inside-callback-function-why-is-it-not-using-th
+		//		// use state updater function
+		//		function updateMessages(oldMessages : any)
+		//		{
+		//			return [...oldMessages, messageElement];
+		//		}
+		//		setMessages(updateMessages);
+		//		console.log(`Received: ${message.body}`);
+		//	}
+		//);
+
 	}
 
 	function initializeConnection()
@@ -56,13 +81,36 @@ function ChatRoom()
 	}
 	function handleMessageSubmission(message : string)
 	{
+		if(!client.current.connected)
+		{
+			return;
+		}
 		let clientMessage = { "text": message };
 		client.current.publish({ destination: `/app/room/${roomName}/publish_message`, body: JSON.stringify(clientMessage) })
 	}
 
-	function handleScrollToTop()
+	function handleScrolledToTop()
 	{
 		console.log("scrolled to top");
+		if(!client.current.connected)
+		{
+			return;
+		}
+		const pageSize : number = 15;
+        let messageRequest =
+        {
+            "creationTimestamp": initTimestamp.current,
+            "requestType": "LESS_THAN_TIMESTAMP",
+            "pageSize": pageSize,
+			"pageNumber": lastRequestedPageNumber.current + 1
+        }
+		client.current.publish
+		(
+			{ 
+				destination: `/app/room/${roomName}/request_messages_by_timestamp`, 
+				body: JSON.stringify(messageRequest) 
+			}
+		)
 	}
 
 	useEffect
@@ -83,7 +131,7 @@ function ChatRoom()
 				{roomName}
 			</div>
 
-			<MessageSection messages={messages} onScrollToTop={handleScrollToTop} />
+			<MessageSection messages={messages} onScrolledToTop={handleScrolledToTop} />
 
 		  	<MessageForm onSubmit={handleMessageSubmission} />
 
--- chat-app/app/src/components/MessageSection/MessageSection.tsx
+++ chat-app/app/src/components/MessageSection/MessageSection.tsx
@@ -1,16 +1,63 @@
+import { useEffect, useRef } from 'react';
 import './MessageSection.css'
 
-function MessageSection({messages, onScrollToTop} : any)
+function MessageSection({messages, onScrolledToTop} : any)
 {
+    const messageSection = useRef<HTMLDivElement>(null);
+    let previousScrollTop = useRef(0);
+    let scrolledToBottom = useRef(true);
+
+
     function handleScroll(event: any)
     {
-        if(event.target.scrollTop === 0)
+        if(messageSection.current == null)
         {
-            onScrollToTop();
+            return;
+        }
+        scrolledToBottom.current = (messageSection.current.scrollHeight - messageSection.current.scrollTop - messageSection.current.clientHeight) < 1;
+        previousScrollTop.current = messageSection.current.scrollTop;
+    }
+    function checkIfScrolledToTop()
+    {
+        if(messageSection.current === null)
+        {
+            return;
+        }
+
+        if(messageSection.current.scrollTop === 0)
+        {
+            onScrolledToTop();
         }
     }
+
+    useEffect
+    (
+        function _()
+        {
+            if(messageSection.current === null)
+            {
+                return;
+            }
+            // if was previously scrolled to the bottom:
+            if(scrolledToBottom.current)
+            {
+                // scroll back to the bottom
+                messageSection.current.scrollTop = messageSection.current.scrollHeight;
+            }
+            else
+            {
+                // go back to the previous scrollTop value (before new messages were added)
+                messageSection.current.scrollTop = previousScrollTop.current;
+            }
+        }
+    )
+    setInterval
+    (
+        checkIfScrolledToTop,
+        1000
+    )
     return (
-    <div id="message-section" onScroll={handleScroll}>
+    <div ref={messageSection} id="message-section" onScroll={handleScroll}>
         {messages}
         {/*
             <img src="/static/chat/loading_icon.svg" alt="Loading..." id="loading-icon">
