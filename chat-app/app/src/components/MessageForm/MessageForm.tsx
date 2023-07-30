b254cd629375098df2d0a32e2ba949060b247a79
none <none>
2023-07-30 20:25:53 +0200

    added behaviour: Submit message on enter

--- chat-app/app/src/components/MessageForm/MessageForm.tsx
+++ chat-app/app/src/components/MessageForm/MessageForm.tsx
@@ -6,6 +6,8 @@ function MessageForm({onSubmit} : any )
 {
 	const [messageFormContent, setMessageFormContent] = useState<string>("");
 	const messageTextArea = useRef<HTMLTextAreaElement>(null);
+
+
 	function handleMessageFormContentChange(event : any)
 	{
 		setMessageFormContent(event.currentTarget.value);
@@ -23,9 +25,34 @@ function MessageForm({onSubmit} : any )
 		setMessageFormContent("");
     }
 
+	function handleKeyDown(event : any)
+	{
+		console.log(event)
+		if(event.keyCode === 13 && event.shiftKey)
+		{
+			return;
+		}
+
+		if(event.keyCode === 13)
+		{
+			event.preventDefault();
+			messageTextArea?.current?.form?.requestSubmit();
+		}
+	}
+
     return (
-		<form id="message-form" onSubmit={onSubmitWrapper}>
-			<textarea form="message-form" id="message-textarea" ref={messageTextArea} rows={1} name="message_text" placeholder="Aa" onChange={handleMessageFormContentChange} value={messageFormContent} />
+		<form id="message-form"  onSubmit={onSubmitWrapper}>
+			<textarea 
+				form="message-form" 
+				id="message-textarea" 
+				placeholder="Aa" 
+				name="message_text" 
+				ref={messageTextArea} 
+				rows={1} 
+				value={messageFormContent} 
+				onChange={handleMessageFormContentChange} 
+				onKeyDown={handleKeyDown}
+			/>
 			<button id="send-button"> </button>
 		</form>
     )
