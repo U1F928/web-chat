6c88d7b4f15f10f7d6e811951399e07b9d91e838
none <none>
2023-08-03 01:40:19 +0200

    reafactored ChatRoom.tsx

--- chat-app/app/src/components/ChatRoom/ChatRoom.tsx
+++ chat-app/app/src/components/ChatRoom/ChatRoom.tsx
@@ -36,12 +36,7 @@ function ChatRoom()
 		const newMessageElement = createNewMessageElement(receivedMessage);
 		//https://stackoverflow.com/questions/59322030/why-is-react-statearray-empty-inside-callback-function-why-is-it-not-using-th
 		// use state updater function
-		function updateMessages(oldMessages : any)
-		{
-			return [...oldMessages, newMessageElement];
-		}
-		setMessages(updateMessages);
-		console.log(`Received: ${message.body}`);
+		setMessages(oldMessages => [...oldMessages, newMessageElement]);
 	}
 
 	function handleRecievedRequestedMessage(message : any)
@@ -57,7 +52,6 @@ function ChatRoom()
 		{
 			return;
 		}
-		console.log(recievedMessages);
 		recievedRequestedMessages.current = true;
 		let newMessageElements : any[] = [];
 		for(let i = 0; i < recievedMessages.length; i++)
@@ -65,14 +59,7 @@ function ChatRoom()
 			const newMessageElement = createNewMessageElement(recievedMessages[i]);
 			newMessageElements.push(newMessageElement);
 		}
-		//https://stackoverflow.com/questions/59322030/why-is-react-statearray-empty-inside-callback-function-why-is-it-not-using-th
-		// use state updater function
-		function updateMessages(oldMessages : any)
-		{
-			return [newMessageElements, ...oldMessages];
-		}
-		setMessages(updateMessages);
-		console.log(`Received: ${message.body}`);
+		setMessages(oldMessages => [newMessageElements, ...oldMessages]);
 	}
 
 	function handleConnect() 
@@ -96,15 +83,10 @@ function ChatRoom()
 		(
 			{
 				brokerURL: `ws://${window.location.hostname}/websocket`,
-				debug: function handleDebug(str : string) 
-				{
-					console.log(str);
-				}
 			}
 		);
 		client.current.onConnect = handleConnect;
 		client.current.activate();
-		console.log("initializing...");
 	}
 
 	function handleMessageSubmission(message : string)
@@ -119,7 +101,6 @@ function ChatRoom()
 
 	function requestOlderMessages()
 	{
-		console.log("scrolled to top");
 		if(!client.current.connected)
 		{
 			return;
@@ -153,7 +134,6 @@ function ChatRoom()
 	(
 		function _()
 		{
-			console.log("was rendered before:" + wasRenderedBefore.current);
 			if(wasRenderedBefore.current === false)
 			{
 				initializeConnection();
